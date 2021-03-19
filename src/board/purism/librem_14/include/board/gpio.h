b1a2dee08ad3d74ea1a54b48ebf15dd84f6f516b
Nicole Faerber <nicole.faerber@puri.sm>
2021-03-19 16:28:34 -0500

    board/librem_14: Adjust GPIOs per schematics
    Signed-off-by: Matt DeVillier <matt.devillier@puri.sm>

--- src/board/purism/librem_14/include/board/gpio.h
+++ src/board/purism/librem_14/include/board/gpio.h
@@ -25,12 +25,11 @@ extern struct Gpio __code DD_ON;
 extern struct Gpio __code EC_EN;
 extern struct Gpio __code EC_RSMRST_N;
 extern struct Gpio __code LAN_WAKEUP_N;
-extern struct Gpio __code LED_ACIN;
 extern struct Gpio __code LED_AIRPLANE_N;
 extern struct Gpio __code LED_BAT_CHG;
-extern struct Gpio __code LED_BAT_FULL;
+extern struct Gpio __code LED_BAT_WARN;
 extern struct Gpio __code LED_PWR;
-extern struct Gpio __code LID_SW_N;
+extern struct Gpio __code LID_SW;
 extern struct Gpio __code PCH_DPWROK_EC;
 extern struct Gpio __code PCH_PWROK_EC;
 extern struct Gpio __code PM_CLKRUN_N;
@@ -40,7 +39,6 @@ extern struct Gpio __code PWR_SW_N;
 extern struct Gpio __code SB_KBCRST_N;
 extern struct Gpio __code SCI_N;
 extern struct Gpio __code SLP_SUS_N;
-extern struct Gpio __code SMI_N;
 extern struct Gpio __code SUSB_N_PCH;
 extern struct Gpio __code SUSC_N_PCH;
 extern struct Gpio __code SUSWARN_N;
@@ -51,6 +49,23 @@ extern struct Gpio __code VA_EC_EN;
 extern struct Gpio __code VR_ON;
 extern struct Gpio __code WLAN_EN;
 extern struct Gpio __code WLAN_PWR_EN;
+extern struct Gpio __code V095A_EN;
+extern struct Gpio __code V095A_PWRGD;
+extern struct Gpio __code V105A_EN;
+extern struct Gpio __code V105A_PWRGD;
+extern struct Gpio __code ALL_SYS_PWRGD_VRON;
+extern struct Gpio __code ROP_VCCST_PWRGD;
+extern struct Gpio __code DDR3VR_PWRGD;
+extern struct Gpio __code PM_SLP_S0_N;
+extern struct Gpio __code PM_SLP_S3_N;
+extern struct Gpio __code PM_SLP_S4_N;
+extern struct Gpio __code BAT_DETECT_N;
+extern struct Gpio __code POWER_ETH_ON;
+extern struct Gpio __code POWER_TP_ON;
+extern struct Gpio __code KBD_BACKLIGHT_EN;
+extern struct Gpio __code SMC_SHUTDOWN_N;
+extern struct Gpio __code CHG_CELL_CFG;
+
 #define HAVE_XLP_OUT 0
 
 #endif // _BOARD_GPIO_H
