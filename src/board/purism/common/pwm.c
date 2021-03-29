9cee61275d83cca17daf6a9d387991e688c2c42f
Nicole Faerber <nicole.faerber@puri.sm>
2021-03-29 19:30:08 +0200

    common/pwm: reduce clock for PWM6 (kbd backlight)

--- src/board/purism/common/pwm.c
+++ src/board/purism/common/pwm.c
@@ -9,15 +9,18 @@ void pwm_init(void) {
     // Disable PWM
     ZTIER = 0;
 
-    // Set prescalar clock frequency to EC clock
+    // Set prescalar clock frequency to EC clock (9.2MHz)
     PCFSR = 0b01;
 
     // Use C0CPRS and CTR0 for all channels
     PCSSGL = 0;
-    PCSSGH = 0;
+    PCSSGH = 0x20;
 
     // Set clock prescaler to 0 + 1
     C0CPRS = 0;
+
+    C6CPRS = 0x01;
+    C6MCPRS = 0x00;
 
     // Set cycle time to 255 + 1
     CTR0 = 255;
