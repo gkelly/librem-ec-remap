78dca81f3a4e474e4186556bd232c1988b2c9030
Nicole Faerber <nicole.faerber@puri.sm>
2022-05-19 17:45:17 +0200

    common/peci: Add methods to set PL1/2/4 via PECI
    Will allow future changes to control power consumption more fine granularly

--- src/board/purism/common/include/board/peci.h
+++ src/board/purism/common/include/board/peci.h
@@ -9,6 +9,9 @@ extern int16_t peci_temp;
 
 void peci_init(void);
 int peci_wr_pkg_config(uint8_t index, uint16_t param, uint32_t data);
+int peci_update_PL1(int watt);
+int peci_update_PL2(int watt);
+int peci_update_PL4(int watt);
 void peci_event(void);
 
 #endif // _BOARD_PECI_H
